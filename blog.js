dd6a7e0e38faad9640f8df23b8b427b256f623ef
Stephen Young <stephenjayyoung@gmail.com>
2015-09-14 10:57:46 -0700

    added some routes and templates. trying to get the public folder, with css and jquery, to work

--- blog.js
+++ blog.js
@@ -1,10 +1,10 @@
 var entries = [
-{"id":1, "title":"Hello World!", "body":"This is the body of my blog entry. Sooo exciting.", "published":"6/2/2013"},
-{"id":2, "title":"Eggs for Breakfast", "body":"Today I had eggs for breakfast. Sooo exciting.", "published":"6/3/2013"},
-{"id":3, "title":"Beer is Good", "body":"News Flash! Beer is awesome!", "published":"6/4/2013"},
-{"id":4, "title":"Mean People Suck", "body":"People who are mean aren't nice or fun to hang around.", "published":"6/5/2013"},
-{"id":5, "title":"I'm Leaving Technology X and You Care", "body":"Let me write some link bait about why I'm not using a particular technology anymore.", "published":"6/10/2013"},
-{"id":6, "title":"Help My Kickstarter", "body":"I want a new XBox One. Please fund my Kickstarter.", "published":"6/12/2013"}];
+{"id":1, "title":"Hello World!", "body":"This is just some stuff."},
+{"id":2, "title":"Things I like", "body":"This is just some more stuff."},
+{"id":3, "title":"Interesting things", "body":"This is even more stuff"},
+{"id":4, "title":"Funny jokes", "body":"Some Stuff, man."},
+{"id":5, "title":"Places to hike", "body":"Good places to go for a cool hike"},
+{"id":6, "title":"Places to cycle", "body":"Good places to get in a good spin.",}];
  
  
 exports.getBlogEntries = function() {
