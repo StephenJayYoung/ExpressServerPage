454d9d2c34ca2b2749d222d671329816fb4bfc60
Stephen Young <stephenjayyoung@gmail.com>
2015-09-14 11:27:56 -0700

    got the styles to work:

--- blog.js
+++ blog.js
@@ -1,10 +1,10 @@
 var entries = [
-{"id":1, "title":"Hello World!", "body":"This is just some stuff."},
-{"id":2, "title":"Things I like", "body":"This is just some more stuff."},
-{"id":3, "title":"Interesting things", "body":"This is even more stuff"},
-{"id":4, "title":"Funny jokes", "body":"Some Stuff, man."},
-{"id":5, "title":"Places to hike", "body":"Good places to go for a cool hike"},
-{"id":6, "title":"Places to cycle", "body":"Good places to get in a good spin.",}];
+{"id":1, "title":"Hello World!", "body":"This is just some stuff.", "published":"09/05/2015"},
+{"id":2, "title":"Things I like", "body":"This is just some more stuff.", "published":"09/05/2015"},
+{"id":3, "title":"Interesting things", "body":"This is even more stuff", "published":"09/05/2015"},
+{"id":4, "title":"Funny jokes", "body":"Some Stuff, man.", "published":"09/05/2015"},
+{"id":5, "title":"Places to hike", "body":"Good places to go for a cool hike", "published":"09/05/2015"},
+{"id":6, "title":"Places to cycle", "body":"Good places to get in a good spin.", "published":"09/05/2015"}];
  
  
 exports.getBlogEntries = function() {
